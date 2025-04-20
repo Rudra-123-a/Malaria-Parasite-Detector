3fa55898f47307f3eb62ad5035f02ae74df0a9e1
Rudra-123-a <143672218+Rudra-123-a@users.noreply.github.com>
2025-04-20 21:51:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ MODEL_PATH = "model/malaria_model.h5"
 # Only download if the model doesn't exist
 if not os.path.exists(MODEL_PATH):
     os.makedirs("model", exist_ok=True)
-    file_id = "1th-9CLlycZelO2nMsBkX2mvDarr0R9vz"  # Replace with your actual file ID
+    file_id = "1gxQzYWm6ChnnB8pOCfzk6uynf_digZjD"  # Replace with your actual file ID
     url = f"https://drive.google.com/uc?id={file_id}"
     print("Downloading model from Google Drive...")
     gdown.download(url, MODEL_PATH, quiet=False)
